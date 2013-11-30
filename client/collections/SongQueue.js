2bd22ae3f5c25cdfa51da7c993dc32375053f641
Lindsey Smith <awkward.lion@gmail.com>
2013-11-29 19:31:21 -0800

    Adding songs triggers the playFirst method properly.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,8 +3,15 @@ window.MyTunes = window.MyTunes || {};
 window.MyTunes.Collections = window.MyTunes.Collections || {};
 
 MyTunes.Collections.SongQueue = MyTunes.Collections.Songs.extend({
-
   initialize: function(){
-  }
+    this.on('add', function(song){
+      if(this.length < 2){
+        this.playFirst(song);
+      }
+    });
+  },
 
+  playFirst: function(){
+
+  }
 });
